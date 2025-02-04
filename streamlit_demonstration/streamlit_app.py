be2bfd1b19b051d937cfd5add9071167f980937b
Vadim Samoilov <metanovus@mail.ru>
2025-02-04 22:03:34 +0300

    Update streamlit_app.py
    Fix 5

--- streamlit_demonstration/streamlit_app.py
+++ streamlit_demonstration/streamlit_app.py
@@ -58,12 +58,17 @@ def start_messaging(rag_top_k=5, max_memory_size=4096):
                 seed=i,
             )
 
+    def clear_text():
+        st.session_state.user_input = st.session_state.widget
+        st.session_state.widget = ""  # Очистить поле ввода
+
     # Поле ввода сообщения
     with st.form(key="chat_form"):
         user_input = st.text_input(
             "Введите сообщение",
             placeholder="Например, какие курсы подходят для аналитиков данных?",
-            key="user_input"
+            key="user_input",
+            on_change=clear_text
         )
         submitted = st.form_submit_button("Отправить")
 
