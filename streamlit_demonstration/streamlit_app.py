94c0111e5888a5f8e0e5f4787bcb55d1ebb2f511
Vadim Samoilov <metanovus@mail.ru>
2025-02-04 22:57:06 +0300

    Update streamlit_app.py
    Fix 26

--- streamlit_demonstration/streamlit_app.py
+++ streamlit_demonstration/streamlit_app.py
@@ -36,11 +36,7 @@ def start_messaging(rag_top_k=5, max_memory_size=4096):
 
     # Кнопка для очистки чата
     if st.button("Очистить чат"):
-        st.session_state.messages = []
-        st.session_state.memory_size = 0
-        st.session_state.context_documents = []
         st.session_state.user_input = ''
-        st.rerun()
 
     # Отображение истории сообщений
     for i, message_data in enumerate(st.session_state.messages):
