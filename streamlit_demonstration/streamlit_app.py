c897f619007e483c0760595036aa3f0ada916d17
Vadim Samoilov <metanovus@mail.ru>
2025-02-04 22:33:18 +0300

    Update streamlit_app.py
    Fix 19

--- streamlit_demonstration/streamlit_app.py
+++ streamlit_demonstration/streamlit_app.py
@@ -68,8 +68,8 @@ def start_messaging(rag_top_k=5, max_memory_size=4096):
         user_input = st.session_state.user_input
         submitted = st.form_submit_button("Отправить")
 
-    if submitted and user_input:
-        st.session_state.user_input = "Введите сообщение"
+    if submitted:
+        st.session_state.user_input = ""
         # Сохраняем сообщение пользователя
         st.session_state.messages.append({"role": "user", "text": user_input})
 
