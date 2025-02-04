107de652b9eb80e2fb33a134c12a1c08619737e1
Vadim Samoilov <metanovus@mail.ru>
2025-02-04 22:41:16 +0300

    Update streamlit_app.py
    Fix 22

--- streamlit_demonstration/streamlit_app.py
+++ streamlit_demonstration/streamlit_app.py
@@ -93,7 +93,7 @@ def start_messaging(rag_top_k=5, max_memory_size=4096):
         # Сохраняем ответ модели
         st.session_state.messages.append({"role": "bot", "text": response})
 
-        # Очищаем поле ввода
+        # Очистим поле ввода после отправки
         st.session_state.user_input = ""
 
         # Перезагружаем интерфейс
